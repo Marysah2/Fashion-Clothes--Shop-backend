bde73639411556fbc489a93dc4fb4782841d8e7c
JosephNjorog <njorojoe11173@gmail.com>
2026-02-13 11:33:06 +0300

    Add explicit table names to User and TokenBlacklist models

--- models/tokenblacklist.py
+++ models/tokenblacklist.py
@@ -1,7 +1,9 @@
-from . import db
+from extensions import db
 from datetime import datetime
 
 class TokenBlacklist(db.Model):
+    __tablename__ = 'token_blacklist'
+    
     id = db.Column(db.Integer, primary_key=True)
     jti = db.Column(db.String(36), nullable=False, unique=True)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
--- models/user.py
+++ models/user.py
@@ -3,6 +3,8 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 
 class User(db.Model):
+    __tablename__ = 'users'
+    
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
