1f029e8cdc78e94ba942ae1433e41c538836a13a
ob002 <amosombworo@gmail.com>
2026-02-13 11:08:15 +0300

    finishing

---
+++ migrations/versions/d8bfb73f5322_merge_heads.py
@@ -0,0 +1,24 @@
+"""merge_heads
+
+Revision ID: d8bfb73f5322
+Revises: 4ae2c6a8a215, 77f2732613b9
+Create Date: 2026-02-13 10:59:46.142184
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'd8bfb73f5322'
+down_revision = ('4ae2c6a8a215', '77f2732613b9')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
